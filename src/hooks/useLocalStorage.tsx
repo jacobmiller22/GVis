bb8ab1590f781fbcbd77fdb4828fd578a07837a5
Jacob Miller <jacobmiller22@vt.edu>
2022-04-27 18:18:04 -0400

    Added optionals for the window object

--- src/hooks/useLocalStorage.tsx
+++ src/hooks/useLocalStorage.tsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 const useLocalStorage = (key: string, initialValue: any) => {
   const [storedValue, setStoredValue] = useState(() => {
     try {
-      const item = window?.localStorage?.getItem(key) ?? initialValue;
+      const item = window?.localStorage?.getItem(key);
       return item ? JSON.parse(item) : initialValue;
     } catch (error) {
       console.error("GET ITEM", error);
